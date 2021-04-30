1f14b58da6881fc7af5676ddf8d441f359779627
John Starich <johnstarich@johnstarich.com>
2021-04-30 01:41:37 -0500

    Switch to env2config

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,23 @@ RUN apt-get update && \
     apt-get clean autoclean && \
     rm -rf /var/lib/apt/* /var/lib/cache/* /var/lib/log/*
 
+
+COPY --from=johnstarich/env2config:v0.1.6 /env2config /
+ENV E2C_CONFIGS=coturn
+
+ENV COTURN_OPTS_FILE=/etc/turnserver.conf
+ENV COTURN_OPTS_FORMAT=ini
+ENV COTURN_OPTS_IN_listening-ip=INTERNAL_IP
+ENV COTURN_OPTS_IN_relay-ip=INTERNAL_IP
+ENV COTURN_OPTS_IN_external-ip=EXTERNAL_IP
+ENV COTURN_OPTS_IN_static-auth-secret=SHARED_SECRET
+ENV COTURN_OPTS_IN_realm=REALM
+ENV COTURN_min-port=65435
+ENV COTURN_max-port=65535
+ENV COTURN_listening-port=3478
+ENV COTURN_use-auth-secret=
+ENV COTURN_no-stdout-log=
+ENV COTURN_log-file=stdout
+
 ENTRYPOINT ["/entrypoint.sh"]
 COPY entrypoint.sh /
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,67 +1,19 @@
 #!/usr/bin/env bash
 # Inspired by https://github.com/boldt/turn-server-docker-image/blob/master/deploy-turnserver.sh
 
-set -e
-
-if [[ -z "$COTURN_SECRET" ]]; then
-    echo "\$COTURN_SECRET is required"
-    exit 2
+set -ex
+if [[ -z "$INTERNAL_IP" ]]; then
+    INTERNAL_IP="$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 fi
-if [[ -z "$COTURN_REALM" ]]; then
-    echo "\$COTURN_REALM is required"
-    exit 2
-fi
-
-MIN_PORT=${MIN_PORT:-65435}
-MAX_PORT=${MAX_PORT:-65535}
-VERBOSE=${VERBOSE:-false}
-COTURN_INTERNAL_IP=${COTURN_INTERNAL_IP}
-COTURN_EXTERNAL_IP=${COTURN_EXTERNAL_IP}
-set -x
-if [[ -z "$COTURN_INTERNAL_IP" ]]; then
-    COTURN_INTERNAL_IP="$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
-fi
-if [[ -z "$COTURN_EXTERNAL_IP" ]]; then
-    COTURN_EXTERNAL_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-    if [[ -z "$COTURN_EXTERNAL_IP" ]]; then
-        COTURN_EXTERNAL_IP="$(dig -4 +short myip.opendns.com @resolver1.opendns.com)"
+if [[ -z "$EXTERNAL_IP" ]]; then
+    EXTERNAL_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+    if [[ -z "$EXTERNAL_IP" ]]; then
+        EXTERNAL_IP="$(dig -4 +short myip.opendns.com @resolver1.opendns.com)"
     fi
 fi
+export INTERNAL_IP EXTERNAL_IP
 set +x
 
-if [[ -z "$COTURN_INTERNAL_IP" ]]; then
-    echo "Failed to get internal IP"
-    exit 1
-fi
-if [[ -z "$COTURN_EXTERNAL_IP" ]]; then
-    echo "Failed to get external IP"
-    exit 1
-fi
-
-template=$(cat <<EOT
-listening-port=3478
-#tls-listening-port=5349
-min-port=$MIN_PORT
-max-port=$MAX_PORT
-listening-ip="$COTURN_INTERNAL_IP"
-relay-ip="$COTURN_INTERNAL_IP"
-external-ip="$COTURN_EXTERNAL_IP"
-
-use-auth-secret
-static-auth-secret="$COTURN_SECRET"
-realm="$COTURN_REALM"
-
-no-stdout-log
-log-file=stdout
-EOT
-)
-if [[ "$VERBOSE" == true ]]; then
-    template+=$'\nverbose'
-fi
-
-set -v
-
-echo "$template" | tee /etc/turnserver.conf | grep -v 'secret'
-
+/env2config
 echo "Starting TURN server..."
 turnserver
